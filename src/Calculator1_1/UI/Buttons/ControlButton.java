1e6f0359fc2b00ef4b9733a4dedc5bba8dc6d9b6
Игорь <igorpekarskij.brest@gmail.com>
2016-09-13 01:57:58 +0300

    Creation buttons

---
+++ src/Calculator1_1/UI/Buttons/ControlButton.java
@@ -0,0 +1,26 @@
+package Calculator1_1.UI.Buttons;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ControlButton extends JButton{
+
+    private static JButton controlButton;
+
+    private final int BUTTON_WIDTH = 50;
+    private final int BUTTON_HEIGHT = 50;
+
+    private ControlButton(String buttonName) {
+        this.controlButton = new JButton(buttonName);
+        controlButton.setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
+
+    }
+
+    public static JButton createButton(String buttonName){
+        new ControlButton(buttonName);
+        return controlButton;
+
+    }
+
+
+}
