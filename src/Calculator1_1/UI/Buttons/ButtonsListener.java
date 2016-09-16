72cb07180496fb98386c70148ad51928a98e59da
igor <igorpekarskij.brest@gmail.com>
2016-09-16 16:12:02 +0300

    Refactored code some, functionality moved in to class compute

--- src/Calculator1_1/UI/Buttons/ButtonsListener.java
+++ src/Calculator1_1/UI/Buttons/ButtonsListener.java
@@ -8,85 +8,17 @@ import java.awt.event.ActionListener;
 public class ButtonsListener implements ActionListener {
 
     private InputOutputField field;
-    private static String enteredNumber ="";
-    private static String firstNumber = "0";
-    private static String secondNumber = "0";
-    private static String operationCharacter = "!";
-    private static int countMinus = 0;
+    private Compute compute;
 
     public ButtonsListener(InputOutputField field) {
-        this.field = field.getIoField();
+        this.field = field.getIOField();
+        this.compute = new Compute();
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         String button = e.getActionCommand();
-        StringBuilder appendChars = new StringBuilder(enteredNumber);
-        if(checkNumber(button) ){
-            enteredNumber = appendChars.append(button).toString();
-            field.setText(enteredNumber);
-        }else if (button.equals(".")){
-            if(enteredNumber.contains(".")){
-                enteredNumber = appendChars.append("").toString();
-                field.setText(enteredNumber);
-            }else {
-                enteredNumber = appendChars.append(button).toString();
-                field.setText(enteredNumber);
-            }
-        }else if (button.equals("CLR")){
-            clear();
-            field.setText(field.getDefaultFieldText());
-
-        }else if(button.equals("=")){
-                displayResult(null);
-
-        }else if(button.equals("%")){
-            displayResult(button);
-
-        }else if(button.equals("minus")){
-            if(countMinus == 0) {
-                enteredNumber = "-" + enteredNumber;
-                field.setText(enteredNumber);
-                countMinus++;
-            }else {
-                enteredNumber = enteredNumber.substring(1);
-                field.setText(enteredNumber);
-                countMinus --;
-            }
-        }
-        // If character equals [+||-||*||/]
-        else {
-            if(enteredNumber != ""){
-                firstNumber = enteredNumber;
-            }
-                operationCharacter = button;
-                enteredNumber = "";
-                countMinus = 0;
-        }
-    }
-    // Method checked is current clicked button with number or not
-    private boolean checkNumber(String button){
-        try {
-            Integer.parseInt(button);
-            return true;
-        }catch (NumberFormatException ignore){
-            return false;
-        }
-    }
-    //Method resets all calculation
-    private void clear(){
-        enteredNumber ="" ;
-        firstNumber = "0";
-        secondNumber = "0";
-        operationCharacter = "!";
-        countMinus = 0;
-    }
-
-    private void displayResult(String percent){
-        secondNumber = enteredNumber;
-        Double result = Compute.compute(firstNumber, secondNumber, operationCharacter, percent);
-        enteredNumber = "";
-        firstNumber = result.toString();
-        field.setText("= " + firstNumber);
+        compute.handleButton(button, field);
     }
 }
