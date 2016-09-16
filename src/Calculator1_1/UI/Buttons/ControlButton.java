e455a8cfb523eec04a0e92c3e942ced77349ccc7
igor <igorpekarskij.brest@gmail.com>
2016-09-16 16:16:25 +0300

    No work was done

--- src/Calculator1_1/UI/Buttons/ControlButton.java
+++ src/Calculator1_1/UI/Buttons/ControlButton.java
@@ -15,8 +15,7 @@ public class ControlButton extends JButton {
     private ControlButton(String buttonName) {
         this.controlButton = new JButton(buttonName);
         controlButton.setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
-        controlButton.addActionListener(new ButtonsListener(InputOutputField.getIoField()));
-
+        controlButton.addActionListener(new ButtonsListener(InputOutputField.getIOField()));
     }
 
     public static JButton createButton(String buttonName){
